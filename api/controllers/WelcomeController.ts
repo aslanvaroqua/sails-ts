952a496eb86531aceb60c36bb5f0fa149821b0f9
Aslan Varoqua <aslan.varoqua@gmail.com>
2017-07-23 09:46:04 -0400

    simple dynamic message from mongo successful

--- api/controllers/WelcomeController.ts
+++ api/controllers/WelcomeController.ts
@@ -12,20 +12,21 @@ import util = require('util');
 declare var sails: any;
 
 const WelcomeController = {
-	index: function (req: e.Request, res: e.Response, next: Function) {
+  index: function (req: e.Request, res: e.Response, next: Function) {
     console.log('index() from WelcomeController.ts');
-    sails.models.welcome
-      .find({
-      })
-      .limit(1)
-      .then(welcome => {
-          res.status(200).send(welcome);
-      })
-      .catch(err => {
-        res.status(200).send(err);
-      })
+// Find the 5 hottest oven brands on the market
+    sails.models.welcome.find().limit(1).then((welcome) => {
 
-	},
+      return res.render('welcome', {
+        welcome: welcome[0].message
+      });
+    }).catch((err) => {
+      return res.status(500).send(err.message);
+    });
+
+
+  },
+
   config: function (req: e.Request, res:e.Response, next:Function) {
     console.log('config() from WelcomeController.ts');
     res.status(200)
--- config/connections.js
+++ config/connections.js
@@ -34,7 +34,7 @@ module.exports.connections = {
 
   localMongoDb: {
     adapter: 'sails-mongo',
-    host: 'localhost', // defaults to `localhost` if omitted
+    // host: 'localhost', // defaults to `localhost` if omitted
     // port: 27017, // defaults to 27017 if omitted
     // user: 'username_here', // or omit if not relevant
     // password: 'password_here', // or omit if not relevant
